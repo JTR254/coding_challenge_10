28640e52836dc9e3d9f7de467fa95e4d560f3c4d
JTR254 <jackrindge@gmail.com>
2025-02-23 22:15:17 -0500

    Task 2 - Creating an Order Class

--- cc_10.js
+++ cc_10.js
@@ -20,3 +20,23 @@ console.log(prod1.getDetails()); // logs the method to the console
 prod1.updateStock(3);
 console.log(prod1.getDetails()); // logs the updated method to the console
 
+// Task 2 - Order Class
+
+class Order { // created Order class
+    constructor (orderId, product, quantity) {
+        this.orderId = orderId
+        this.product = product;
+        this.quantity = quantity;
+        this.product.updateStock(quantity); // used updateStock method to adjust the quantity after the order was placed
+    }
+    getOrderDetails(){ // created method to gather the order details
+        const totalPrice = this.product.price * this.quantity // allows totalPrice to equal the product price * the quantity
+        return `Order ID: ${this.orderId}, Product: ${this.product.name}, Quantity: ${this.quantity}, Total Price $${totalPrice}`
+    }
+};
+const order1 = new Order(501, prod1, 2);
+console.log(order1.getOrderDetails()); 
+// Output: "Order ID: 501, Product: Laptop, Quantity: 2, Total Price: $2400"
+
+console.log(prod1.getDetails()); 
+// Output: "Product: Laptop, ID: 101, Price: $1200, Stock: 5"
